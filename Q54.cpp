92058b84cb4a21b7fd8342f87276de14e20ecdd2
nancyrawat038 <nancyrawat26dec2005@gmail.com>
2026-06-29 03:00:47 +0530

    Add files via upload

---
+++ Q54.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int n,x,count=0;
+    cin >> n;
+    int a[n];
+    for (int i=0; i<n; i++)
+    cin >> a[i];
+    cin >> x;
+    for(int i=0; i<n; i++)
+    if(a[i]==x)
+    count++;
+    cout << count;
+}
